4eff579b296b9e62dd8ab475eb29b5e827fba542
Alwin Arrasyid <alwin.ridd@gmail.com>
2024-11-20 15:47:47 +0700

    rename service name

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ from .observability.tracing import setup_tracing, instrument_app
 from .llms.chat.route import router as chat_router
 from .llms.agent.route import router as agent_router
 
-service_name = "langchain-otel2"
+service_name = "langchain-tracing-example"
 tracer_provider = setup_tracing(service_name, "http://localhost:4318/v1/traces")
 
 app = FastAPI()
